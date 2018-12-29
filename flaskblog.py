5972f91a7d7a1ef7b6e6cfe3df5dc3e3a12f7dc5
Ray Yuan <ray.x.yuan@gmail.com>
2018-12-28 20:48:08 -0800

    set up jinja2 templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts=[
+    {
+        'name':'Ray',
+        'content':'test post 1 please work',
+        'date':'12/28/2018'
+    },
+    {
+        'name':'Kevin',
+        'content':'test post 2 this is the second post.',
+        'date':'12/29/2018'
+    },
+    {
+        'name':'Kevin',
+        'content':'test post 3 this is the second post.',
+        'date':'12/31/2018'
+    }
+]
 @app.route("/")
-@app.route("/temp")
-def hello():
-    return "<h1>hellaefao</h1>"
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=posts,title='TESTBITCH')
 
 @app.route("/about")
 def about():
-    return "<h1>aboutpage</h1>"
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
